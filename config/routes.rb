7aeaf31a2bf27d1ef004bbd8ec3b3f59c48499c9
davigl <davi.guimaraesleite@gmail.com>
2018-03-16 16:48:35 -0300

    Adicionado CRUD usuario nexti

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :usuario_nextis
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match 'welcome' => 'welcome#index', via: [:get]
 
---
+++ test/models/usuario_nexti_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UsuarioNextiTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
