05fc63f2fd4577b4de8245ae78b829016e3645b2
Marcus Vinicius <marcusviniciusmorais@gmail.com>
2018-03-22 15:46:48 -0300

    Criada o scaffold do telefone

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :telefones
   devise_for :usuario_nextis
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match 'welcome' => 'welcome#index', via: [:get]
