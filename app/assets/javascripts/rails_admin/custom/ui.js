339f598400bf118eec0aefe08707ac6a4573ae18
Victor <victorsouza449@gmail.com>
2018-04-16 16:54:03 -0300

    backg

--- app/assets/javascripts/rails_admin/custom/ui.js
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -1,60 +1,3 @@
-
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
 $(document).on('ready pjax:success', function() {
   handleActiveBase();
   function handleActiveBase() {
@@ -85,7 +28,7 @@ $(function () {
     }
   });
  
-  for (var i = 0; i &lt;= array_menu.length; i++) {
+  for (var i = 0; i = array_menu.length; i++) {
     $('.sub-menu-' + i).wrapAll("&lt;div class='sub-menu-container' /&gt;");
   }
  
--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,15 @@ Bundler.require(*Rails.groups)
 ENV['RAILS_ADMIN_THEME'] = 'material_theme'
 
 module Nextidoacao
-  class Application < Rails::Application
+	class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.default_locale = 'pt-BR'
 
+    config.assets.enabled = true
+    config.assets.path << Rails.root.join('/app/assets/fonts')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  end
+end
 end
