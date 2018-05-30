3a2515ac84f5a8919efd437d150c0b6d367afaf1
Victor <victorsouza449@gmail.com>
2018-05-30 15:35:54 -0300

    template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
 	def show
-    	render template: "pages/#{params[:page]}"
+    	if params[:page] == "teste"
+    		render template: "pages/#{params[:page]}", :layout => false
+    	else
+    		render template: "pages/#{params[:page]}"
+    	end
   	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,9 +20,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match 'not-actived' => 'pages#show', via: [:get], :as => :user_not_actived, page: "not-actived"
   match 'email-validation' => 'pages#show', via: [:get], :as => :email_validation, page: "email-validation"
-
   match 'welcome' => 'pages#show', via: [:get], :as => :welcome, page: "welcome"
-
   match 'cadastrar-instituicao' => 'instituicaos#new', via: [:get], :as => :cadastrar_instituicao
 
   get "/:page" => "pages#show"
