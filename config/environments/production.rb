657cde4ecff9604e586af92b7bad0d8bd604c3e5
Davi <davi.guimaraesleite@gmail.com>
2018-06-04 17:25:21 -0300

    smpt settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "nextidoacao_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.smtp_settings = {
+    :user_name => 'nextipro2@gmail.com',
+    :password => 'AdminEnel2017',
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+    :domain => 'gmail.com'
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
