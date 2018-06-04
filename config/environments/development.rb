d90094519876bf5b96a78d24b2dad25b86e565cb
Davi <davi.guimaraesleite@gmail.com>
2018-06-04 17:22:37 -0300

    changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
     :enable_starttls_auto => true,
     :domain => 'gmail.com'
   }
-  
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
