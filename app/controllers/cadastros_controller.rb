e766e1d8c419a97defaa8aa35fee86e8df482ac6
Davi <davi.guimaraesleite@gmail.com>
2018-06-25 15:45:11 -0300

    Removendo Scaffold Css

--- app/controllers/cadastros_controller.rb
+++ app/controllers/cadastros_controller.rb
@@ -1,7 +1,7 @@
 class CadastrosController < ApplicationController
   
-  before_action :set_cadastro, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :set_cadastro, only: [:show, :edit, :update, :destroy]
 
   # GET /cadastros
   # GET /cadastros.json
