951cb8d57215da06555b78481d6675286de208e2
Victor <victorsouza449@gmail.com>
2018-06-28 14:27:23 -0300

    finalizada área para cadastro de doadores

--- app/controllers/cadastros_controller.rb
+++ app/controllers/cadastros_controller.rb
@@ -1,7 +1,7 @@
 class CadastrosController < ApplicationController
   
-  before_action :authenticate_user!
   before_action :set_cadastro, only: [:show, :edit, :update, :destroy]
+  layout false
 
   # GET /cadastros
   # GET /cadastros.json
