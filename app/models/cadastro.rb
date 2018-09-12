3352a5f16c6cae4b72554fb2d9b9508ad869d77f
Victor <victorsouza449@gmail.com>
2018-09-12 17:52:00 -0300

    @davigl barra de pesquisa

--- app/models/cadastro.rb
+++ app/models/cadastro.rb
@@ -1,7 +1,7 @@
 class Cadastro < ApplicationRecord
 	belongs_to :user
 	belongs_to :instituicao
-	paginates_per 15
+	paginates_per 10
 
 	validates_presence_of :id_cliente_enel, :digito_verificador_cliente_enel, :codigo_ocorrencia, :valor, :parcelas
 	validates :id_cliente_enel, length: { maximum: 10, too_long: "é maior que o aceitável de 10 digitos. Verifique e tente novamente." }
@@ -19,6 +19,14 @@ class Cadastro < ApplicationRecord
 		end
 
 		return colaboradores
+	end
+
+	def self.search(query)
+		if query.present?
+			where(['id_cliente_enel::text LIKE :query OR autor::text LIKE :query', query: "%#{query}"])
+		else
+			all
+		end
 	end
 
 end
