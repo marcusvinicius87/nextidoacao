b96c8ecc88a6653fbaf2698164d7fe2067bb572b
Davi <davi.guimaraesleite@gmail.com>
2018-10-09 17:18:33 -0300

    Mudanças no dashboard

--- app/assets/javascripts/dashboard/jquery.dashboard.js
+++ app/assets/javascripts/dashboard/jquery.dashboard.js
@@ -11,52 +11,6 @@ $( document ).ready(function() {
         dias = [dia.domingo, dia.segunda, dia.terca, dia.quarta, dia.quinta, dia.sexta, dia.sabado];
     });
 
-<<<<<<< HEAD
-    var DrawSparkline = function() {     
-            $('#sparkline1').sparkline(dias, {
-                type: 'bar',
-                tooltipFormat: '{{offset:offset}}: {{value}}',
-                tooltipValueLookups: {
-                    'offset': {
-                        0: 'Domingo',
-                        1: 'Segunda',
-                        2: 'Terca',
-                        3: 'Quarta',
-                        4: 'Quinta',
-                        5: 'Sexta',
-                        6: 'Sabado'
-                    }
-                },
-                height: '200',
-                barWidth: '10',
-                barSpacing: '5',
-                barColor: '#ffffff'
-            });
-
-            $('#sparkline2').sparkline(meses, {
-                type: 'bar',
-                height: '200',
-                barWidth: '10',
-                barSpacing: '5',
-                barColor: '#3bafda',
-                tooltipFormat: '{{offset:offset}}: {{value}}',
-                tooltipValueLookups: {
-                    'offset': {
-                        0: 'Janeiro ',
-                        1: 'Fevereiro',
-                        2: 'Março',
-                        3: 'Abril ',
-                        4: 'Maio ',
-                        5: 'Junho ',
-                        6: 'Julho ',
-                        7: 'Agosto ',
-                        8: 'Setembro ',
-                        9: 'Outubro ',
-                        10: 'Novembro ',
-                        11: 'Dezembro '
-                    }
-=======
-
     var DrawSparkline = function() {
         $('#sparkline1').sparkline(dias, {
             type: 'bar',
@@ -70,7 +24,6 @@ $( document ).ready(function() {
                     4: 'Quinta',
                     5: 'Sexta',
                     6: 'Sabado'
->>>>>>> fecb56973601c2842fe3335aa1a7a24d09e11f6c
                 }
             },
             height: '200',
@@ -103,18 +56,6 @@ $( document ).ready(function() {
                 }
             }
         });
-
-<<<<<<< HEAD
-            $('#sparkline3').sparkline([20, 40, 30, 10], {
-                type: 'pie',
-                width: '200',
-                height: '200',
-                sliceColors: ['#dcdcdc', '#3bafda', '#333333', '#00b19d']
-            });
-
-        };
-=======
->>>>>>> fecb56973601c2842fe3335aa1a7a24d09e11f6c
 
         $('#sparkline3').sparkline([20, 40, 30, 10], {
             type: 'pie',
