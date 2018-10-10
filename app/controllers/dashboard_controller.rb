52f80754d0a2db3ee50d360dd79073f40cc4a70d
Davi <davi.guimaraesleite@gmail.com>
2018-10-10 16:52:34 -0300

    Modificações das legendas dos gráficos

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,41 +1,50 @@
 class DashboardController < ApplicationController
 	layout false
 	before_action :authenticate_user!
-
+	
 	def index
+		@dashboard = Dashboard.new
+
 		@instituicao = current_user.instituicao
 		@cadastros_dia = current_user.instituicao.cadastros.where('extract(day from created_at) = ?', Date.today.day).size
 		@cadastros_mes = current_user.instituicao.cadastros.select('valor').where('extract(month from data_ocorrencia) = ? AND extract(year from data_ocorrencia) = ? AND doador_ativo = ?', Date.today.month, Date.today.year, true)
 		
 		@ultimos_cadastros = current_user.instituicao.cadastros.last(5)
-		@arrecadado = get_arrecadado(@cadastros_mes)
-		@percentual = (@cadastros_dia * 100) / 50
-		@percentual_liquido = (@arrecadado * 100) / 1000
+		@arrecadado = @dashboard.get_arrecadado(@cadastros_mes)
+		@percentual = @dashboard.percentual(@cadastros_dia)
+		@percentual_liquido = @dashboard.percentual_liquido(@arrecadado)
+
+		@semana_values = get_semana_values
+		@meses_values = get_mes_values
+
+		@dia_mais_cadastros = @dashboard.maior_hash_key(@semana_values)[0]
+		@dia_menos_cadastros = @dashboard.menor_hash_key(@semana_values)[0]
+	
+		@mes_mais_cadastros = @dashboard.maior_hash_key(@meses_values)[0]
+		@mes_menos_cadastros = @dashboard.menor_hash_key(@meses_values)[0]
 	end
 
 	def get_cadastros_mes
-		meses = []
-		data = current_user.instituicao.cadastros
-
-		(1..12).each { |mes| meses << data.select("data_ocorrencia").where('extract(month from data_ocorrencia) = ? AND extract(year from data_ocorrencia) = ?', mes, Date.today.year).size }
-		
-		render json: {janeiro: meses[0], fevereiro: meses[1], marco: meses[2], abril: meses[3], maio: meses[4], 
-					junho: meses[5], julho: meses[6], agosto: meses[7], setembro: meses[8], outubro: meses[9], novembro: meses[10], dezembro: meses[11]}, status: :ok
+		render json: get_mes_values, status: :ok
 	end
 
 	def get_cadastros_semana
-		dias = []
-		data = current_user.instituicao.cadastros.select("data_ocorrencia").where('data_ocorrencia between ? and ?', Date.today.beginning_of_week, Date.today.end_of_week)
-
-		(0..6).each { |dia| dias << data.where('extract(dow from data_ocorrencia) = ?', dia).size}
-
-		render json: {domingo: dias[0], segunda: dias[1], terca: dias[2], quarta: dias[3], quinta: dias[4], 
-					sexta: dias[5], sabado: dias[6]}, status: :ok
+		render json: get_semana_values, status: :ok
 	end
 
-	def get_arrecadado (values)
-		arrecadado = 0
-		values.each { |cadastro| arrecadado += cadastro.valor }
-		return arrecadado
-	end
+	private
+		def get_mes_values
+			data = current_user.instituicao.cadastros
+			meses = Dashboard.get_values_months(data)
+			hash_values = {janeiro: meses[0], fevereiro: meses[1], marco: meses[2], abril: meses[3], maio: meses[4], 
+						junho: meses[5], julho: meses[6], agosto: meses[7], setembro: meses[8], outubro: meses[9], novembro: meses[10], dezembro: meses[11]}
+		end
+
+		def get_semana_values
+			data = current_user.instituicao.cadastros.select("data_ocorrencia").where('data_ocorrencia between ? and ?', Date.today.beginning_of_week, Date.today.end_of_week)
+			dias = Dashboard.get_values_days(data)
+			hash_values = {domingo: dias[0], segunda: dias[1], terca: dias[2], quarta: dias[3], quinta: dias[4], 
+						sexta: dias[5], sabado: dias[6]}
+		end
+
 end
--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,13 +1,35 @@
-def Dashboard
-	def initialize
-
+class Dashboard
+	def maior_hash_key(hash)
+  		hash.max_by{|k,v| v}
 	end
 
-	def get_cadastros_mes
-		
+	def menor_hash_key(hash)
+  		hash.min_by{|k,v| v}
 	end
 
-	def get_cadastros_semana(data)
+	def self.get_values_months(data)
+		meses = []
+		(1..12).each { |mes| meses << data.select("data_ocorrencia").where('extract(month from data_ocorrencia) = ? AND extract(year from data_ocorrencia) = ?', mes, Date.today.year).size }
+		meses
+	end
 
+	def self.get_values_days(data)
+		dias = []
+		(0..6).each { |dia| dias << data.where('extract(dow from data_ocorrencia) = ?', dia).size}
+		dias
+	end
+
+	def get_arrecadado (values)
+		arrecadado = 0
+		values.each { |cadastro| arrecadado += cadastro.valor }
+		arrecadado
+	end
+
+	def percentual (cadastros_dia)
+		return (cadastros_dia * 100) / 50
+	end
+
+	def percentual_liquido (arrecadado)
+		return (arrecadado * 100) / 1000
 	end
 end
