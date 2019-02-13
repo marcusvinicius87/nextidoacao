94de6b691f435055271ab2f92ada9aca0450e787
Victor <victorsouza449@gmail.com>
2019-02-13 15:01:34 -0300

    refatorando código

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,8 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( custom.css )
+Rails.application.config.assets.precompile += %w( dashboard/* )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
